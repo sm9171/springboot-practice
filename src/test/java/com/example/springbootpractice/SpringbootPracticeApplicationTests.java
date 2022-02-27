1a0301fc98f6b0f34b4928f45fc95fc2bdf2e9a5
sm9171 <sm9171@nate.com>
2022-02-27 23:49:12 +0900

    refactor(ci/cd): 깃헙액션 수정

--- src/test/java/com/example/springbootpractice/SpringbootPracticeApplicationTests.java
+++ src/test/java/com/example/springbootpractice/SpringbootPracticeApplicationTests.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"jasypt.encryptor.password=sunday8turtle"})
 @ActiveProfiles("local")
 class SpringbootPracticeApplicationTests {
 
