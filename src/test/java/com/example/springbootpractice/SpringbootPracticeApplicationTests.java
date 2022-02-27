d8b99bc6db69992efc214d06f5f0c14a52bce210
sm9171 <sm9171@nate.com>
2022-02-28 00:53:31 +0900

    refactor(ci/cd): 깃헙액션 수정

--- src/test/java/com/example/springbootpractice/SpringbootPracticeApplicationTests.java
+++ src/test/java/com/example/springbootpractice/SpringbootPracticeApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("ci")
+@ActiveProfiles("local")
 class SpringbootPracticeApplicationTests {
 
 	@Test
